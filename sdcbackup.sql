1cddf9949f700e0cad96dc5c8efd37d3ea149d0f
Tristan Deitzer <tristandeitzer@gmail.com>
2020-04-13 18:16:00 -0400

    Quick update

--- sdcbackup.sql
+++ sdcbackup.sql
@@ -16,26 +16,36 @@
 -------------------- CREATE CHARACTERISTICS TABLE --------------------
 
 CREATE TABLE public.characteristic (
-    characteristic_id serial NOT NULL,
+    characteristic_id integer NOT NULL,
     char_name_id integer,
     value jsonb
 );
 
 ALTER TABLE public.characteristic OWNER TO administrator;
 
+CREATE SEQUENCE characteristic_characteristic_id_seq;
+SELECT setval('characteristic_characteristic_id_seq', 3374748);
+ALTER TABLE characteristic ALTER COLUMN characteristic_id SET DEFAULT
+nextval('characteristic_characteristic_id_seq'::regclass);
+
 -------------------- CREATE CHARACTERISTIC_NAMES TABLE --------------------
 
 CREATE TABLE public.characteristic_names (
-    char_name_id serial NOT NULL,
+    char_name_id integer NOT NULL,
     name character varying(60) NOT NULL
 );
 
 ALTER TABLE public.characteristic_names OWNER TO administrator;
 
+CREATE SEQUENCE characteristic_names_char_name_id_seq;
+SELECT setval('characteristic_names_char_name_id_seq', 6);
+ALTER TABLE characteristic_names ALTER COLUMN char_name_id SET DEFAULT
+nextval('characteristic_names_char_name_id_seq'::regclass);
+
 -------------------- CREATE CHARACTERISTIC_SET TABLE --------------------
 
 CREATE TABLE public.characteristic_set (
-    product_id serial,
+    product_id integer,
     characteristic_id integer
 );
 
@@ -44,27 +54,37 @@ ALTER TABLE public.characteristic_set OWNER TO administrator;
 -------------------- CREATE PHOTOS TABLE --------------------
 
 CREATE TABLE public.photos (
-    review_id serial NOT NULL,
+    review_id integer NOT NULL,
     photos jsonb
 );
 
 ALTER TABLE public.photos OWNER TO administrator;
 
+CREATE SEQUENCE photos_review_id_seq;
+SELECT setval('photos_review_id_seq', 5777922);
+ALTER TABLE photos ALTER COLUMN review_id SET DEFAULT
+nextval('photos_review_id_seq'::regclass);
+
 -------------------- CREATE PRODUCT_METADATA TABLE --------------------
 
 CREATE TABLE public.product_metadata (
-    product_id serial NOT NULL,
+    product_id integer NOT NULL,
     ratings jsonb,
     recommended jsonb
 );
 
 ALTER TABLE public.product_metadata OWNER TO administrator;
 
+CREATE SEQUENCE product_metadata_product_id_seq;
+SELECT setval('product_metadata_product_id_seq', 1000010);
+ALTER TABLE product_metadata ALTER COLUMN product_id SET DEFAULT
+nextval('product_metadata_product_id_seq'::regclass);
+
 -------------------- CREATE PRODUCT_REVIEWS TABLE --------------------
 
 CREATE TABLE public.product_reviews (
-    review_id serial NOT NULL,
-    product_id serial NOT NULL
+    review_id integer NOT NULL,
+    product_id integer NOT NULL
 );
 
 ALTER TABLE public.product_reviews OWNER TO administrator;
@@ -72,7 +92,7 @@ ALTER TABLE public.product_reviews OWNER TO administrator;
 -------------------- CREATE PRODUCTS TABLE --------------------
 
 CREATE TABLE public.products (
-    product_id serial NOT NULL
+    product_id integer NOT NULL
 );
 
 ALTER TABLE public.products OWNER TO administrator;
@@ -80,7 +100,7 @@ ALTER TABLE public.products OWNER TO administrator;
 -------------------- CREATE REVIEWS TABLE --------------------
 
 CREATE TABLE public.reviews (
-    review_id serial NOT NULL,
+    review_id integer NOT NULL,
     rating integer,
     summary character varying(1000),
     response character varying(1000),
@@ -94,6 +114,11 @@ CREATE TABLE public.reviews (
 );
 
 ALTER TABLE public.reviews OWNER TO administrator;
+
+CREATE SEQUENCE reviews_review_id_seq;
+SELECT setval('reviews_review_id_seq', 5777922);
+ALTER TABLE reviews ALTER COLUMN review_id SET DEFAULT
+nextval('reviews_review_id_seq'::regclass);
 
 -------------------- CREATE PRIMARY KEY CONSTRAINTS --------------------
 
@@ -121,3 +146,9 @@ GRANT ALL ON TABLE public.reviews TO hackreactor;
 -- PostgreSQL database dump complete
 --
 
+-- CREATE INDEX reviews_index ON reviews (review_id);
+-- CREATE INDEX products_index ON products (product_id);
+-- CREATE INDEX product_reviews_index ON product_reviews (product_id, review_id);
+-- CREATE INDEX product_metadata_index ON product_metadata (product_id);
+-- CREATE INDEX photos_index ON photos (review_id);
+-- CREATE INDEX characteristic_index ON characteristic (characteristic_id);
